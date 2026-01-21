79d05b6a4aa479d1eb16a04f34b95690636bca43
Matthijs <bomaltennishal@outlook.com>
2026-01-21 18:44:28 +0100

    nieuw

---
+++ src/test/java/io/matthijs/service/ContentService/ContentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package io.matthijs.service.ContentService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContentServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
